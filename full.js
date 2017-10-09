bb04f98d435c79935ac25da36999b137e9a27512
jaykaron <jaykaron@gmail.com>
2017-10-08 23:49:33 -0400

    Files and Music
    Spilt up files and added a concat script.
    Got music icons.

---
+++ full.js
@@ -0,0 +1,272 @@
+
+var background; // Background Image
+var pc;         // The player obj
+var platforms = [];   //Array holding all the platform obj.s
+var startTime;
+
+var clearPlatforms; // A boolean true when a platform is off the screen to the left
+
+var vText;            // TEMP - TextPoint obj. showing the velocity
+var timeText;
+var speedText;
+
+var music = new Audio("ChoazFantasy.mp3");
+var soundButton;    //A path, clicking it toggles the music
+
+var gameSpeed = 1;
+
+function init() {
+
+  initBackground();
+  initHUD();
+  initMusic();
+
+  pc = new PC(400,50);
+
+  platforms.push(new Platform(new Rectangle(300,275, 250,30), 1.5,0));
+  for(var i=0; i<8; i++)
+    newPlatform();
+
+  startTime = Date.now();
+
+}
+function initBackground(){
+  background = new Raster("background");
+  background.position = view.center;
+}
+function initHUD() {
+  vText = new PointText(10,25);
+  timeText = new PointText(10,50);
+  speedText = new PointText(10,75);
+
+
+  soundButton = new Raster("soundOn");
+  soundButton.position = new Point(950, 550);
+  soundButton.box = new Path.Rectangle(925,525, 50,50);
+}
+function initMusic() {
+  music.play();
+  music.loop = true;
+
+}
+
+// The GAME LOOP
+function onFrame(event) {
+  pc.move();
+  for (var i=0; i<platforms.length; i++)
+    platforms[i].move();
+
+  if(clearPlatforms) {
+    if (platforms[0].offScreen) {
+      platforms.shift();
+      newPlatform();
+    }
+  }
+  showInfo();   //Updates the vText
+}
+function showInfo() {
+  vText.content = "V: " + pc.v;
+  var timePassed =  Math.round((Date.now() - startTime)/1000);
+  if(timePassed % 15 == 0) {
+    gameSpeed = 1 + 0.1 * (timePassed / 15);
+    console.log("SPEED UP "+gameSpeed);
+  }
+  if(timePassed > timeText.content)
+    timeText.content = timePassed;
+  speedText.content = "Speed: "+gameSpeed;
+}
+
+function newPlatform() {
+  var lastPlat = platforms[platforms.length-1];
+  var newOriginPoint;
+
+  do {
+    newOriginPoint = lastPlat.box.topRight+new Point(randomInt(-100,150), randomInt(-200,200));
+  } while (newOriginPoint.y > 550 || newOriginPoint.y < 50);
+  platforms.push(new Platform(new Rectangle(newOriginPoint, new Size(randomInt(100,350), 30)),1.5, randomInt(0,3)));
+}
+function randomInt(min, max) {
+  return Math.floor(Math.random()*(max-min)+min);
+}
+function onKeyDown(event){
+  switch (event.key) {
+    case 'f':
+      pc.nextC();
+      break;
+      case 'd':
+        pc.prevC();
+        break;
+    default:
+      ;
+  }
+}
+
+function onKeyUp(event) {
+  if(event.key = 'space')
+    pc.peaked = true;
+}
+
+function onMouseDown(event) {
+  if(soundButton.box.contains(event.point)) {
+    if(music.paused) {
+      music.play();
+      soundButton.image = document.getElementById("soundOn");
+    }
+    else {
+      music.pause();
+      soundButton.image = document.getElementById("soundOff");
+    }
+  }
+}
+
+init();
+
+// Start of PC.js
+
+function PC(x, y) {
+    this.w = 35; this.h = 35;
+
+    this.v = 0; this.a = 0.5;   // Velocity, Acceleration
+    this.MAX_V = 25;
+
+    this.c = 0;     // Color
+
+    this.peaked = false;
+    this.onPlatform = false;
+
+    this.box = new Rectangle(x,y, this.w,this.h);
+    this.path = new Path.Rectangle(this.box);
+    this.path.fillColor = "red";
+    this.path.strokeColor = "black";
+
+    this.move = function() {
+
+      this.updateV();
+
+      this.shift(this.v);
+
+      var currentPlatform = this.checkPlatforms();
+      if(currentPlatform) {
+        this.shift(-this.box.intersect(currentPlatform.box).height);
+      }
+    }
+
+    this.shift = function(deltaY) {
+      this.box.y += deltaY;
+      var vector = new Point(0, deltaY);
+      this.path.position += vector;
+    }
+
+    this.nextC = function() {
+      /* Switches to the next color */
+      this.c = (this.c + 1) % 3;
+      this.updateC();
+    }
+
+    this.prevC = function() {
+      /* Switches to the previous color */
+      this.c = (this.c + 2) % 3;
+      this.updateC();
+    }
+    this.updateC = function() {
+      switch (this.c) {
+        case 0:
+          this.path.fillColor = "red";
+          break;
+        case 1:
+          this.path.fillColor = "blue";
+          break;
+        case 2:
+          this.path.fillColor = "green";
+          break;
+        default:
+          this.path.fillColor = "red";
+      }
+    }
+
+    this.checkPlatforms = function() {
+      if(this.v < 0) {
+        this.onPlatform = false;
+        return null;
+      }
+      for (var i=0; i<platforms.length; i++) {
+        if (platforms[i].c == this.c) {
+          if(platforms[i].box.contains(this.box.bottomRight) || platforms[i].box.contains(this.box.bottomLeft)) {
+            this.onPlatform = true;
+            this.peaked = false;
+            return platforms[i];
+          }
+        }
+      }
+      this.onPlatform = false;
+      return null;
+    }
+
+    this.updateV = function() {
+      if(Key.isDown('space') && !this.peaked) {
+        if(this.onPlatform && this.v >= 0) {
+      		this.v -= 5;
+      	}
+      	else if (!this.peaked && this.v < 0) {
+      		this.v -= 1;
+      	}
+        if(this.v <- 15 || this.v > 0) {
+      		this.peaked = true;
+      	}
+      }
+      else if(!this.onPlatform) {
+          this.v += this.a;
+      }
+      else if (this.v > 0) {
+        this.v = 0;
+      }
+      if(this.v > 25)
+        this.v = this.MAX_V;
+    }
+}
+
+// End of PC.js
+
+// Start of Platform.js
+
+function Platform(rect, v, c) {
+  this.move = function() {
+    this.box.x -= v * gameSpeed;
+    if(this.box.topRight.x < 0) {
+      clearPlatforms = true;
+      this.offScreen = true;
+    }
+
+    var vector = new Point(-v*gameSpeed, 0);
+    this.path.position += vector;
+  }
+
+  this.updateC = function() {
+    switch (this.c) {
+      case 0:
+        this.path.fillColor = "red";
+        break;
+      case 1:
+        this.path.fillColor = "blue";
+        break;
+      case 2:
+        this.path.fillColor = "green";
+        break;
+      default:
+        this.path.fillColor = "red";
+    }
+  }
+
+  this.box = rect;
+  this.path = new Path.Rectangle(this.box);
+  this.path.strokeColor = "black";
+
+  this.v = v; this.c = c;
+
+  this.offScreen = false;
+
+  this.updateC();
+
+}
+
+// End of Platform.js
--- scripts/PC.js
+++ scripts/PC.js
@@ -1,49 +1,106 @@
+
+// Start of PC.js
+
 function PC(x, y) {
-    this.x = x;  this.y = y;
     this.w = 35; this.h = 35;
 
-    this.v = 0; this.a = 1;   // Velocity, Acceleration
+    this.v = 0; this.a = 0.5;   // Velocity, Acceleration
+    this.MAX_V = 25;
+
     this.c = 0;     // Color
 
-    this.jumping = false;
-    this.onPlatform = true;
+    this.peaked = false;
+    this.onPlatform = false;
 
-    this.bottomBox = new Rectangle(x,y+((3*h)/4),w,h/4);
+    this.box = new Rectangle(x,y, this.w,this.h);
+    this.path = new Path.Rectangle(this.box);
+    this.path.fillColor = "red";
+    this.path.strokeColor = "black";
 
-    this.movePC = function() {
-      /* Checks if PC intersects platform
-     	 * if not move PC down
-     	 * && statements make it so you can jump up from under platforms
-     	 */
-     	 	if(onPlatform==false){
-        		v=v+a;
-        		y=y+v;
-        		bottomBox.y=y+((3*h)/4);
-        		onPlatform = false;
-     	 	}
+    this.move = function() {
+
+      this.updateV();
+
+      this.shift(this.v);
+
+      var currentPlatform = this.checkPlatforms();
+      if(currentPlatform) {
+        this.shift(-this.box.intersect(currentPlatform.box).height);
+      }
     }
 
-    this.increaseV = function () {
-    /* First does a hop if on platform
-     * Then slowly increases velocity, until a cap */
-    	if(onPlatform==true){
-    		v=v-9;
-    	}
-    	else if(jumping==true){
-    		v=v-2;;
-    	}
-    	if(v<-19){
-    		jumping =false;
-    	}
+    this.shift = function(deltaY) {
+      this.box.y += deltaY;
+      var vector = new Point(0, deltaY);
+      this.path.position += vector;
     }
 
     this.nextC = function() {
       /* Switches to the next color */
-      c = (c + 1) % 3;
+      this.c = (this.c + 1) % 3;
+      this.updateC();
     }
 
     this.prevC = function() {
       /* Switches to the previous color */
-      c = (c + 2) % 3;
+      this.c = (this.c + 2) % 3;
+      this.updateC();
+    }
+    this.updateC = function() {
+      switch (this.c) {
+        case 0:
+          this.path.fillColor = "red";
+          break;
+        case 1:
+          this.path.fillColor = "blue";
+          break;
+        case 2:
+          this.path.fillColor = "green";
+          break;
+        default:
+          this.path.fillColor = "red";
+      }
+    }
+
+    this.checkPlatforms = function() {
+      if(this.v < 0) {
+        this.onPlatform = false;
+        return null;
+      }
+      for (var i=0; i<platforms.length; i++) {
+        if (platforms[i].c == this.c) {
+          if(platforms[i].box.contains(this.box.bottomRight) || platforms[i].box.contains(this.box.bottomLeft)) {
+            this.onPlatform = true;
+            this.peaked = false;
+            return platforms[i];
+          }
+        }
+      }
+      this.onPlatform = false;
+      return null;
+    }
+
+    this.updateV = function() {
+      if(Key.isDown('space') && !this.peaked) {
+        if(this.onPlatform && this.v >= 0) {
+      		this.v -= 5;
+      	}
+      	else if (!this.peaked && this.v < 0) {
+      		this.v -= 1;
+      	}
+        if(this.v <- 15 || this.v > 0) {
+      		this.peaked = true;
+      	}
+      }
+      else if(!this.onPlatform) {
+          this.v += this.a;
+      }
+      else if (this.v > 0) {
+        this.v = 0;
+      }
+      if(this.v > 25)
+        this.v = this.MAX_V;
     }
 }
+
+// End of PC.js
