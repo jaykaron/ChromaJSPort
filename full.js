efb6f8c2993ecedd082fb5fd118cc54005f3b859
jaykaron <jaykaron@gmail.com>
2017-10-29 20:49:52 -0400

    Tweaked Credit Placement and Wording

--- full.js
+++ full.js
@@ -219,12 +219,12 @@ function initWelcome() {
     
     var controlsText = makeText(view.center+new Point(0, 130), largeTextStyle, 20, "SPACE to jump.   F and D to switch colors.");
 
-    var authorText = makeText(new Point(20, screenHeight - 75), smallTextStyle, 16, "Game by Jay Karon");
+    var authorText = makeText(new Point(20, screenHeight - 55), smallTextStyle, 16, "Made by Jay Karon");
     makeLink(authorText, "https://github.com/jaykaron");
     
-    var originalText = makeText(new Point(20, screenHeight - 58), smallTextStyle, 12, "Original design by Ari Karon");
+    var originalText = makeText(new Point(20, screenHeight - 38), smallTextStyle, 12, "Game design by Ari Karon");
     
-    var musicText = makeText(new Point(20, screenHeight - 40), smallTextStyle, 12, "Music: Chaoz Fantasy by ParagonX9");
+    var musicText = makeText(new Point(20, screenHeight - 20), smallTextStyle, 12, "Music: Chaoz Fantasy by ParagonX9");
     makeLink(musicText, "https://soundcloud.com/paragonx9");
   mainLayer.activate();
 }
