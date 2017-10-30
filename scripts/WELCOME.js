eef10b67b95e8e28d15995e47cfa127357c9983f
jaykaron <jaykaron@gmail.com>
2017-10-29 20:36:40 -0400

    Added Linked Credits
    Also made a helper function to create PointText, cleaned up a lot of code.

--- scripts/WELCOME.js
+++ scripts/WELCOME.js
@@ -12,14 +12,16 @@ function initWelcome() {
     var title = new Raster("title");
     title.position = view.center;
     
-    var spaceText = new PointText(view.center+new Point(0,95));
-    spaceText.style = largeTextStyle;
-    spaceText.style.fontSize = 35;
-    spaceText.content = "Press SPACE to start";
+    var spaceText = makeText(view.center+new Point(0, 95), largeTextStyle, 35, "Press SPACE to start");
     
-    var controlsText = new PointText(view.center+new Point(0,130));
-    controlsText.style = largeTextStyle;
-    controlsText.style.fontSize = 20;
-    controlsText.content = "SPACE to jump.   F and D to switch colors.";
+    var controlsText = makeText(view.center+new Point(0, 130), largeTextStyle, 20, "SPACE to jump.   F and D to switch colors.");
+
+    var authorText = makeText(new Point(20, screenHeight - 75), smallTextStyle, 16, "Game by Jay Karon");
+    makeLink(authorText, "https://github.com/jaykaron");
+    
+    var originalText = makeText(new Point(20, screenHeight - 58), smallTextStyle, 12, "Original design by Ari Karon");
+    
+    var musicText = makeText(new Point(20, screenHeight - 40), smallTextStyle, 12, "Music: Chaoz Fantasy by ParagonX9");
+    makeLink(musicText, "https://soundcloud.com/paragonx9");
   mainLayer.activate();
 }
